04fb05794631ded737c9517f2e4adb68b68f0cc4
Michael Barall <gituser@invisiblesoft.com>
2018-03-26 17:25:27 -0700

    AAFS server part 1

---
+++ src/scratch/aftershockStatistics/aafs/entity/LogEntry.java
@@ -0,0 +1,458 @@
+package scratch.aftershockStatistics.aafs.entity;
+
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.Index;
+import org.mongodb.morphia.annotations.Indexed;
+import org.mongodb.morphia.annotations.IndexOptions;
+import org.mongodb.morphia.annotations.Indexes;
+import org.mongodb.morphia.annotations.Field;
+import org.mongodb.morphia.annotations.Transient;
+
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.FindAndModifyOptions;
+
+import org.mongodb.morphia.query.Query;
+import org.mongodb.morphia.query.UpdateOperations;
+
+import org.json.simple.JSONObject;
+
+import scratch.aftershockStatistics.aafs.MongoDBUtil;
+import scratch.aftershockStatistics.aafs.RecordKey;
+
+
+
+
+/**
+ * Holds an AAFS log entry in the MongoDB database.
+ * Author: Michael Barall 03/15/2018.
+ *
+ * The collection "log" holds the log entries.
+ */
+@Entity("log")
+@Indexes({
+	@Index(fields = {@Field("event_id")}, options = @IndexOptions(name = "logevid")),
+	@Index(fields = {@Field("log_time")}, options = @IndexOptions(name = "logtime"))
+})
+public class LogEntry implements java.io.Serializable {
+
+	//----- Envelope information -----
+
+	// Globally unique identifier for this log entry.
+	// This is the MongoDB identifier.
+	// Note that ObjectId implements java.io.Serializable.
+	// This is set to the same value as the id of the task that generated the log entry.
+
+    @Id
+    private ObjectId id;
+
+	// Time that this log entry was created, in milliseconds since the epoch.
+	// The collection is indexed on this field, so log entries in a given
+	// time span can be obtained with a database query efficiently.
+
+	//@Indexed(options = @IndexOptions(name = "logtime"))
+	private long log_time;
+
+	//----- Task information -----
+
+	// Event ID that this task pertains to.
+	// Entries not referring to an event should put an empty string here (not null).
+	// This can be used to find all log entries pertaining to an event.
+
+	//@Indexed(options = @IndexOptions(name = "eventid"))
+	private String event_id;
+
+	// Time this task was originally scheduled to execute, in milliseconds since the epoch.
+
+	private long sched_time;
+
+	// Time this task was submitted, in milliseconds since the epoch.
+
+	private long submit_time;
+
+	// Person or entity that submitted the task.
+
+	private String submit_id;
+
+	// Operation code for this task.
+
+	private int opcode;
+
+	// Stage number, assigned by the user.
+	// This can be used for a sequence of commands with the same PendingTask.
+
+	private int stage;
+
+	// Details of this task.
+	// Any additional information neeed is stored as a string.
+	// If none, this should be an empty string (not null).
+
+	private String details;
+
+	//----- Result information -----
+
+	// Result code for this task.
+
+	private int rescode;
+
+	// Results for this task.
+	// If none, this should be an empty string (not null).
+
+	private String results;
+
+
+
+
+	//----- Getters and setters -----
+
+    private ObjectId get_id() {
+        return id;
+    }
+
+    private void set_id (ObjectId id) {
+        this.id = id;
+    }
+
+    public long get_log_time() {
+        return log_time;
+    }
+
+    private void set_log_time (long log_time) {
+        this.log_time = log_time;
+    }
+
+    public String get_event_id() {
+        return event_id;
+    }
+
+    private void set_event_id (String event_id) {
+        this.event_id = event_id;
+    }
+
+    public long get_sched_time() {
+        return sched_time;
+    }
+
+    private void set_sched_time (long sched_time) {
+        this.sched_time = sched_time;
+    }
+
+    public long get_submit_time() {
+        return submit_time;
+    }
+
+    private void set_submit_time (long submit_time) {
+        this.submit_time = submit_time;
+    }
+
+    public String get_submit_id() {
+        return submit_id;
+    }
+
+    private void set_submit_id (String submit_id) {
+        this.submit_id = submit_id;
+    }
+
+    public int get_opcode() {
+        return opcode;
+    }
+
+    private void set_opcode (int opcode) {
+        this.opcode = opcode;
+    }
+
+    public int get_stage() {
+        return stage;
+    }
+
+    private void set_stage (int stage) {
+        this.stage = stage;
+    }
+
+    public String get_details() {
+        return details;
+    }
+
+    private void set_details (String details) {
+        this.details = details;
+    }
+
+    public int get_rescode() {
+        return rescode;
+    }
+
+    private void set_rescode (int rescode) {
+        this.rescode = rescode;
+    }
+
+    public String get_results() {
+        return results;
+    }
+
+    private void set_results (String results) {
+        this.results = results;
+    }
+
+
+
+
+	// toString - Convert to string.
+
+	@Override
+	public String toString() {
+		String str = "LogEntry\n"
+			+ "\tid: " + ((id == null) ? ("null") : (id.toHexString())) + "\n"
+			+ "\tlog_time: " + log_time + "\n"
+			+ "\tevent_id: " + event_id + "\n"
+			+ "\tsched_time: " + sched_time + "\n"
+			+ "\tsubmit_time: " + submit_time + "\n"
+			+ "\tsubmit_id: " + submit_id + "\n"
+			+ "\topcode: " + opcode + "\n"
+			+ "\tstage: " + stage + "\n"
+			+ "\tdetails: " + details + "\n"
+			+ "\trescode: " + rescode + "\n"
+			+ "\tresults: " + results;
+		return str;
+	}
+
+
+
+
+	/**
+	 * get_record_key - Get the record key for this log entry.
+	 */
+	public RecordKey get_record_key () {
+		return new RecordKey(id);
+	}
+
+
+
+
+	/**
+	 * set_record_key - Set the record key for this log entry.
+	 * @param key = Record key. Can be null.
+	 */
+	private void set_record_key (RecordKey key) {
+		if (key == null) {
+			id = null;
+		} else {
+			id = key.getId();
+		}
+		return;
+	}
+
+
+
+
+	/**
+	 * submit_log_entry - Submit a log entry.
+	 * @param key = Record key associated with this task. Can be null to assign a new one.
+	 * @param log_time = Time of this log entry, in milliseconds
+	 *                   since the epoch. Must be positive.
+	 * @param event_id = Event associated with this task, or "" if none. Cannot be null.
+	 * @param sched_time = Time at which task should execute, in milliseconds
+	 *                     since the epoch. Must be positive.
+	 * @param submit_time = Time at which the task is submitted, in milliseconds
+	 *                      since the epoch. Must be positive.
+	 * @param submit_id = Person or entity submitting this task. Cannot be empty or null.
+	 * @param opcode = Operation code used to dispatch the task.
+	 * @param stage = Stage number, user-defined, effectively an extension of the opcode.
+	 * @param details = Further details of this task, or "" if none. Cannot be null.
+	 * @param rescode = Result code.
+	 * @param results = Further results of this task, or "" if none. Cannot be null.
+	 * @return
+	 */
+	public static void submit_log_entry (RecordKey key, long log_time, String event_id,
+			long sched_time, long submit_time, String submit_id, int opcode, int stage,
+			String details, int rescode, String results) {
+
+		// Check conditions
+
+		if (!( log_time > 0L
+			&& event_id != null
+			&& sched_time > 0L
+			&& submit_time > 0L
+			&& submit_id != null && submit_id.length() > 0
+			&& details != null
+			&& results != null )) {
+			throw new RuntimeException("LogEntry.submit_log_entry: Invalid log parameters");
+		}
+
+		// Construct the log entry object
+
+		LogEntry lentry = new LogEntry();
+		lentry.set_record_key (key);
+		lentry.set_log_time (log_time);
+		lentry.set_event_id (event_id);
+		lentry.set_sched_time (sched_time);
+		lentry.set_submit_time (submit_time);
+		lentry.set_submit_id (submit_id);
+		lentry.set_opcode (opcode);
+		lentry.set_stage (stage);
+		lentry.set_details (details);
+		lentry.set_rescode (rescode);
+		lentry.set_results (results);
+
+		// Call MongoDB to store into database
+
+		Datastore datastore = MongoDBUtil.getDatastore();
+		datastore.save(lentry);
+		
+		return;
+	}
+
+
+
+
+	/**
+	 * submit_log_entry - Submit a log entry.
+	 * @param ptask = Pending task record associated with this task. Cannot be null.
+	 * @param log_time = Time of this log entry, in milliseconds
+	 *                     since the epoch. Must be positive.
+	 * @param rescode = Result code.
+	 * @param results = Further results of this task, or "" if none. Cannot be null.
+	 * @return
+	 * Other log parameters are copied from ptask.
+	 */
+	public static void submit_log_entry (PendingTask ptask, long log_time, int rescode, String results) {
+
+		// Check conditions
+
+		if (!( ptask != null
+			&& log_time > 0L
+			&& results != null )) {
+			throw new RuntimeException("LogEntry.submit_log_entry: Invalid log parameters");
+		}
+
+		// Submit the log entry
+
+		submit_log_entry (
+			ptask.get_record_key(),
+			log_time,
+			ptask.get_event_id(),
+			ptask.get_sched_time(),
+			ptask.get_submit_time(),
+			ptask.get_submit_id(),
+			ptask.get_opcode(),
+			ptask.get_stage(),
+			ptask.get_details(),
+			rescode,
+			results);
+		
+		return;
+	}
+
+
+
+
+	/**
+	 * get_log_entry_for_key - Get the log entry with the given key.
+	 * @param key = Record key. Cannot be null or empty.
+	 * Returns the log entry, or null if not found.
+	 */
+	public static LogEntry get_log_entry_for_key (RecordKey key) {
+
+		if (!( key != null && key.getId() != null )) {
+			throw new RuntimeException("LogEntry.get_log_entry_for_key: Missing or empty record key");
+		}
+
+		// Get the MongoDB data store
+
+		Datastore datastore = MongoDBUtil.getDatastore();
+
+		// Construct the query: Select id == key
+
+		Query<LogEntry> query = datastore.createQuery(LogEntry.class)
+											.filter("id ==", key.getId());
+
+		// Run the query
+
+		LogEntry lentry = query.get();
+
+		return lentry;
+	}
+
+
+
+
+	/**
+	 * get_log_entry_range - Get a range of log entries, reverse-sorted by log time.
+	 * @param log_time_lo = Minimum log time, in milliseconds since the epoch.
+	 *                      Can be 0L for no minimum.
+	 * @param log_time_hi = Maximum log time, in milliseconds since the epoch.
+	 *                      Can be 0L for no maximum.
+	 * @param event_id = Event id. Can be null to return entries for all events.
+	 */
+	public static List<LogEntry> get_log_entry_range (long log_time_lo, long log_time_hi, String event_id) {
+
+		// Get the MongoDB data store
+
+		Datastore datastore = MongoDBUtil.getDatastore();
+
+		// Construct the query
+
+		Query<LogEntry> query = datastore.createQuery(LogEntry.class);
+
+		// Select by event_id
+
+		if (event_id != null) {
+			query = query.filter("event_id ==", event_id);
+		}
+
+		// Select entries with log_time >= log_time_lo
+
+		if (log_time_lo > 0L) {
+			query = query.filter("log_time >=", new Long(log_time_lo));
+		}
+
+		// Select entries with log_time <= log_time_hi
+
+		if (log_time_hi > 0L) {
+			query = query.filter("log_time <=", new Long(log_time_hi));
+		}
+
+		// Sort by log_time in descending order (most recent first)
+
+		query = query.order("-log_time");
+
+		// Run the query
+
+		List<LogEntry> entries = query.asList();
+
+		return entries;
+	}
+
+
+
+
+	/**
+	 * delete_log_entry - Delete a log entry.
+	 * @param entry = Existing log entry to delete.
+	 * @return
+	 */
+	public static void delete_log_entry (LogEntry entry) {
+
+		// Check conditions
+
+		if (!( entry != null && entry.get_id() != null )) {
+			throw new RuntimeException("LogEntry.delete_log_entry: Invalid parameters");
+		}
+
+		// Get the MongoDB data store
+
+		Datastore datastore = MongoDBUtil.getDatastore();
+
+		// Run the delete
+
+		datastore.delete(entry);
+		
+		return;
+	}
+
+
+
+
+}
---
+++ src/scratch/aftershockStatistics/aafs/entity/PendingTask.java
@@ -0,0 +1,489 @@
+package scratch.aftershockStatistics.aafs.entity;
+
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.Index;
+import org.mongodb.morphia.annotations.Indexed;
+import org.mongodb.morphia.annotations.IndexOptions;
+import org.mongodb.morphia.annotations.Indexes;
+import org.mongodb.morphia.annotations.Field;
+import org.mongodb.morphia.annotations.Transient;
+
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.FindAndModifyOptions;
+
+import org.mongodb.morphia.query.Query;
+import org.mongodb.morphia.query.UpdateOperations;
+
+import org.json.simple.JSONObject;
+
+import scratch.aftershockStatistics.aafs.MongoDBUtil;
+import scratch.aftershockStatistics.aafs.RecordKey;
+
+
+
+
+/**
+ * Holds a pending AAFS task in the MongoDB database.
+ * Author: Michael Barall 03/15/2018.
+ *
+ * The collection "tasks" holds the queue of pending tasks.
+ */
+@Entity("tasks")
+@Indexes({
+	@Index(fields = {@Field("event_id")}, options = @IndexOptions(name = "eventid")),
+	@Index(fields = {@Field("exec_time")}, options = @IndexOptions(name = "extime"))
+})
+public class PendingTask implements java.io.Serializable {
+
+	//----- Envelope information -----
+
+	// Globally unique identifier for this task.
+	// This is the MongoDB identifier.
+	// Note that ObjectId implements java.io.Serializable.
+
+    @Id
+    private ObjectId id;
+
+	// Time that this task is scheduled to execute, in milliseconds since the epoch.
+	// The collection is indexed on this field, so the task with the earliest
+	// time can be obtained with a database query efficiently.
+	// When beginning to execute a task, this field is set to zero, which guarantees
+	// it will be seen again if the task is interrupted and restarted.
+
+	//@Indexed(options = @IndexOptions(name = "extime"))
+	private long exec_time;
+
+	//----- Task information -----
+
+	// Event ID that this task pertains to.
+	// Tasks not referring to an event should put an empty string here (not null).
+	// This can be used to find all queued tasks pertaining to an event.
+
+	//@Indexed(options = @IndexOptions(name = "eventid"))
+	private String event_id;
+
+	// Time this task was originally scheduled to execute, in milliseconds since the epoch.
+
+	private long sched_time;
+
+	// Time this task was submitted, in milliseconds since the epoch.
+
+	private long submit_time;
+
+	// Person or entity that submitted the task.
+
+	private String submit_id;
+
+	// Operation code for this task.
+
+	private int opcode;
+
+	// Stage number, assigned by the user.
+	// This can be used for a sequence of commands with the same PendingTask.
+
+	private int stage;
+
+	// Details of this task.
+	// Any additional information neeed is stored as a string.
+	// If none, this should be an empty string (not null).
+
+	private String details;
+
+
+
+
+	//----- Getters and setters -----
+
+    private ObjectId get_id() {
+        return id;
+    }
+
+    private void set_id (ObjectId id) {
+        this.id = id;
+    }
+
+    private long get_exec_time() {
+        return exec_time;
+    }
+
+    private void set_exec_time (long exec_time) {
+        this.exec_time = exec_time;
+    }
+
+    public String get_event_id() {
+        return event_id;
+    }
+
+    private void set_event_id (String event_id) {
+        this.event_id = event_id;
+    }
+
+    public long get_sched_time() {
+        return sched_time;
+    }
+
+    private void set_sched_time (long sched_time) {
+        this.sched_time = sched_time;
+    }
+
+    public long get_submit_time() {
+        return submit_time;
+    }
+
+    private void set_submit_time (long submit_time) {
+        this.submit_time = submit_time;
+    }
+
+    public String get_submit_id() {
+        return submit_id;
+    }
+
+    private void set_submit_id (String submit_id) {
+        this.submit_id = submit_id;
+    }
+
+    public int get_opcode() {
+        return opcode;
+    }
+
+    private void set_opcode (int opcode) {
+        this.opcode = opcode;
+    }
+
+    public int get_stage() {
+        return stage;
+    }
+
+    private void set_stage (int stage) {
+        this.stage = stage;
+    }
+
+    public String get_details() {
+        return details;
+    }
+
+    private void set_details (String details) {
+        this.details = details;
+    }
+
+
+
+
+	// toString - Convert to string.
+
+	@Override
+	public String toString() {
+		String str = "PendingTask\n"
+			+ "\tid: " + ((id == null) ? ("null") : (id.toHexString())) + "\n"
+			+ "\texec_time: " + exec_time + "\n"
+			+ "\tevent_id: " + event_id + "\n"
+			+ "\tsched_time: " + sched_time + "\n"
+			+ "\tsubmit_time: " + submit_time + "\n"
+			+ "\tsubmit_id: " + submit_id + "\n"
+			+ "\topcode: " + opcode + "\n"
+			+ "\tstage: " + stage + "\n"
+			+ "\tdetails: " + details;
+		return str;
+	}
+
+
+
+
+	/**
+	 * get_record_key - Get the record key for this pending task.
+	 * Each pending task is assigned a unique record key when the task is submitted.
+	 * The key remains the same when the task is activated or when a new stage begins.
+	 */
+	public RecordKey get_record_key () {
+		return new RecordKey(id);
+	}
+
+
+
+
+	/**
+	 * is_restarted - Return true if this task has been restarted.
+	 * Restarted means that a previous attempt was made to execute the task,
+	 * but it failed before deleting the task from the queue.
+	 */
+	public boolean is_restarted () {
+		return exec_time == 0L;
+	}
+
+
+
+
+	/**
+	 * get_apparent_time - Return the apparent execution time of this command.
+	 * Note: This is primarily for accelerated testing.
+	 */
+	public long get_apparent_time () {
+		return (exec_time == 0L) ? sched_time : exec_time;
+	}
+
+
+
+
+	/**
+	 * submit_task - Submit a task.
+	 * @param event_id = Event associated with this task, or "" if none. Cannot be null.
+	 * @param sched_time = Time at which task should execute, in milliseconds
+	 *                     since the epoch. Must be positive.
+	 * @param submit_time = Time at which the task is submitted, in milliseconds
+	 *                      since the epoch. Must be positive.
+	 * @param submit_id = Person or entity submitting this task. Cannot be empty or null.
+	 * @param opcode = Operation code used to dispatch the task.
+	 * @param stage = Stage number, user-defined, effectively an extension of the opcode.
+	 * @param details = Further details of this task, or "" if none. Cannot be null.
+	 * @return
+	 */
+	public static void submit_task (String event_id, long sched_time, long submit_time,
+								String submit_id, int opcode, int stage, String details) {
+
+		// Check conditions
+
+		if (!( event_id != null
+			&& sched_time > 0L
+			&& submit_time > 0L
+			&& submit_id != null && submit_id.length() > 0
+			&& details != null )) {
+			throw new RuntimeException("PendingTask.submit_task: Invalid task parameters");
+		}
+
+		// Construct the pending task object
+
+		PendingTask ptask = new PendingTask();
+		ptask.set_id (null);
+		ptask.set_exec_time (sched_time);
+		ptask.set_event_id (event_id);
+		ptask.set_sched_time (sched_time);
+		ptask.set_submit_time (submit_time);
+		ptask.set_submit_id (submit_id);
+		ptask.set_opcode (opcode);
+		ptask.set_stage (stage);
+		ptask.set_details (details);
+
+		// Call MongoDB to store into database
+
+		Datastore datastore = MongoDBUtil.getDatastore();
+		datastore.save(ptask);
+		
+		return;
+	}
+
+
+
+
+	/**
+	 * get_all_tasks_unsorted - Get a list of all pending tasks, without sorting.
+	 * This is primarily for testing and monitoring.
+	 */
+	public static List<PendingTask> get_all_tasks_unsorted () {
+
+		// Get the MongoDB data store
+
+		Datastore datastore = MongoDBUtil.getDatastore();
+
+		// Construct the query: Select documents of type PendingTask
+
+		Query<PendingTask> query = datastore.createQuery(PendingTask.class);
+
+		// Run the query
+
+		List<PendingTask> tasks = query.asList();
+
+		return tasks;
+	}
+
+
+
+
+	/**
+	 * get_all_tasks - Get a list of all pending tasks, sorted by execution time.
+	 * This is primarily for testing and monitoring.
+	 */
+	public static List<PendingTask> get_all_tasks () {
+
+		// Get the MongoDB data store
+
+		Datastore datastore = MongoDBUtil.getDatastore();
+
+		// Construct the query: Sort by exec_time
+
+		Query<PendingTask> query = datastore.createQuery(PendingTask.class)
+											.order("exec_time");
+
+		// Run the query
+
+		List<PendingTask> tasks = query.asList();
+
+		return tasks;
+	}
+
+
+
+
+	/**
+	 * get_first_task - Get the first task, that is, the task with smallest execution time.
+	 * This is primarily for testing and monitoring.
+	 */
+	public static PendingTask get_first_task () {
+
+		// Get the MongoDB data store
+
+		Datastore datastore = MongoDBUtil.getDatastore();
+
+		// Construct the query: Sort by exec_time
+
+		Query<PendingTask> query = datastore.createQuery(PendingTask.class)
+											.order("exec_time");
+
+		// Run the query
+
+		PendingTask task = query.get();
+
+		return task;
+	}
+
+
+
+
+	/**
+	 * get_first_ready_task - Get the first ready task, according to execution time.
+	 * @param cutoff_time = Cutoff time, in milliseconds since the epoch.
+	 * Only tasks with exec_time <= cutoff_time are considered.
+	 * Return is null if there are no such tasks.
+	 * This is primarily for testing and monitoring.
+	 */
+	public static PendingTask get_first_ready_task (long cutoff_time) {
+
+		// Get the MongoDB data store
+
+		Datastore datastore = MongoDBUtil.getDatastore();
+
+		// Construct the query: Select exec_time <= cutoff_time, sort by exec_time
+
+		Query<PendingTask> query = datastore.createQuery(PendingTask.class)
+											.filter("exec_time <=", new Long(cutoff_time))
+											.order("exec_time");
+
+		// Run the query
+
+		PendingTask task = query.get();
+
+		return task;
+	}
+
+
+
+
+	/**
+	 * activate_first_ready_task - Get and activate the first ready task, according to execution time.
+	 * @param cutoff_time = Cutoff time, in milliseconds since the epoch.
+	 * Only tasks with exec_time <= cutoff_time are considered.
+	 * Return is null if there are no such tasks.
+	 * The task is marked active by setting exec_time = 0 in the database.
+	 */
+	public static PendingTask activate_first_ready_task (long cutoff_time) {
+
+		// Get the MongoDB data store
+
+		Datastore datastore = MongoDBUtil.getDatastore();
+
+		// Construct the query: Select exec_time <= cutoff_time, sort by exec_time
+
+		Query<PendingTask> query = datastore.createQuery(PendingTask.class)
+											.filter("exec_time <=", new Long(cutoff_time))
+											.order("exec_time");
+
+		// Construct the update operation: Set exec_time to 0L
+
+		UpdateOperations<PendingTask> update_op
+				= datastore.createUpdateOperations(PendingTask.class)
+							.set("exec_time", new Long(0L));
+
+		// Construct the find and modify options: Return the original document value
+
+		FindAndModifyOptions modify_opt = (new FindAndModifyOptions())
+											.returnNew(false);
+
+		// Run the query
+
+		PendingTask task = datastore.findAndModify(query, update_op, modify_opt);
+
+		return task;
+	}
+
+
+
+
+	/**
+	 * stage_task - Begin a new stage of a task.
+	 * @param ptask = Existing pending task to stage.
+	 * @param exec_time = Time at which task should execute, in milliseconds
+	 *                    since the epoch. Must be positive.
+	 * @param stage = Stage number, user-defined, effectively an extension of the opcode.
+	 * @return
+	 */
+	public static void stage_task (PendingTask ptask, long exec_time, int stage) {
+
+		// Check conditions
+
+		if (!( ptask != null && ptask.get_id() != null
+			&& exec_time > 0L )) {
+			throw new RuntimeException("PendingTask.stage_task: Invalid task parameters");
+		}
+
+		// Get the MongoDB data store
+
+		Datastore datastore = MongoDBUtil.getDatastore();
+
+		// Construct the update operations: Set exec_time and stage
+
+		UpdateOperations<PendingTask> update_op
+				= datastore.createUpdateOperations(PendingTask.class)
+							.set("exec_time", new Long(exec_time))
+							.set("stage", new Integer(stage));
+
+		// Run the update
+
+		datastore.update(ptask, update_op);
+		
+		return;
+	}
+
+
+
+
+	/**
+	 * delete_task - Delete a task.
+	 * @param ptask = Existing pending task to delete.
+	 * @return
+	 */
+	public static void delete_task (PendingTask ptask) {
+
+		// Check conditions
+
+		if (!( ptask != null && ptask.get_id() != null )) {
+			throw new RuntimeException("PendingTask.delete_task: Invalid task parameters");
+		}
+
+		// Get the MongoDB data store
+
+		Datastore datastore = MongoDBUtil.getDatastore();
+
+		// Run the delete
+
+		datastore.delete(ptask);
+		
+		return;
+	}
+
+
+
+
+}
