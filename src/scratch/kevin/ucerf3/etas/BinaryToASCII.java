7f209b50656978355be64aeb0b90b73dedb684af
Kevin Milner <kmilner@usc.edu>
2018-03-29 09:39:49 -0700

    now handles directory input

--- src/scratch/kevin/ucerf3/etas/BinaryToASCII.java
+++ src/scratch/kevin/ucerf3/etas/BinaryToASCII.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import org.opensha.commons.util.FileNameComparator;
+
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 
 import scratch.UCERF3.erf.ETAS.ETAS_CatalogIO;
 import scratch.UCERF3.erf.ETAS.ETAS_EqkRupture;
@@ -25,14 +28,39 @@ public class BinaryToASCII {
 		Preconditions.checkArgument(inputFile.exists());
 		Preconditions.checkArgument(outputDir.exists() || outputDir.mkdir());
 		
-		int count = 0;
-		for (List<ETAS_EqkRupture> catalog : ETAS_CatalogIO.getBinaryCatalogsIterable(inputFile, -1)) {
-			File file = new File(outputDir, "catalog_"+count+".txt");
-			ETAS_CatalogIO.writeEventDataToFile(file, catalog);
-			
-			count++;
-			if (num > 0 && count == num)
-				break;
+		if (inputFile.isDirectory()) {
+			List<File> subDirs = Lists.newArrayList(inputFile.listFiles());
+			for (int i=subDirs.size(); --i>=0;) {
+				File subDir = subDirs.get(i);
+				if (!subDir.isDirectory() || !subDir.getName().startsWith("sim_"))
+					subDirs.remove(i);
+			}
+			subDirs.sort(new FileNameComparator());
+			if (num < 0)
+				num = subDirs.size();
+			else if (subDirs.size() < num)
+				num = subDirs.size();
+			for (int i=0; i<num; i++) {
+				File simInFile = new File(subDirs.get(i), "simulatedEvents.bin");
+				Preconditions.checkState(simInFile.exists());
+				File outputFile;
+				if (inputFile.equals(outputDir))
+					outputFile = new File(subDirs.get(i), "simulatedEvents.txt");
+				else
+					outputFile = new File(outputDir, "catalog_"+i+".txt");
+				List<ETAS_EqkRupture> catalog = ETAS_CatalogIO.loadCatalogBinary(simInFile);
+				ETAS_CatalogIO.writeEventDataToFile(outputFile, catalog);
+			}
+		} else {
+			int count = 0;
+			for (List<ETAS_EqkRupture> catalog : ETAS_CatalogIO.getBinaryCatalogsIterable(inputFile, -1)) {
+				File file = new File(outputDir, "catalog_"+count+".txt");
+				ETAS_CatalogIO.writeEventDataToFile(file, catalog);
+				
+				count++;
+				if (num > 0 && count == num)
+					break;
+			}
 		}
 	}
 
