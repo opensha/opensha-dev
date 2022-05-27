891ec1f7f37899fc7d86669ca3cae6d29d43115a
Kevin Milner <kmilner@usc.edu>
2022-05-27 10:33:08 -0700

    undid this empty class

--- src/main/java/scratch/kevin/nshm23/classicModel/ClassicModelSolver.java
+++
@@ -1,9 +0,0 @@
-package scratch.kevin.nshm23.classicModel;
-
-import org.opensha.sha.earthquake.faultSysSolution.FaultSystemSolution;
-
-public class ClassicModelSolver {
-	
-//	public static FaultSystemSolution solve(double fractGR, boolean charIs)
-
-}
