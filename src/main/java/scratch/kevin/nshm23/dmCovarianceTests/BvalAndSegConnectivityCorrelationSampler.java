ef3bc5d0a21d6e700cf7000297393be0046849f8
Kevin Milner <kmilner@usgs.gov>
2026-01-14 21:22:32 -0800

    this doesn't compile with java 17, fixed

--- src/main/java/scratch/kevin/nshm23/dmCovarianceTests/BvalAndSegConnectivityCorrelationSampler.java
+++ src/main/java/scratch/kevin/nshm23/dmCovarianceTests/BvalAndSegConnectivityCorrelationSampler.java
@@ -60,9 +60,9 @@ public class BvalAndSegConnectivityCorrelationSampler extends SectionCovarianceS
 			SupraSeisBValInversionTargetMFDs.Builder builder = new SupraSeisBValInversionTargetMFDs.Builder(rupSet, bVal);
 			if (segModel != null) {
 				JumpProbabilityCalc segModelCalc = segModel.getModel(rupSet, null);
-				if (segModelCalc instanceof BinaryJumpProbabilityCalc)
-					builder.forBinaryRupProbModel((BinaryJumpProbabilityCalc)segModel);
-				else
+//				if (segModelCalc instanceof BinaryJumpProbabilityCalc)
+//					builder.forBinaryRupProbModel((BinaryJumpProbabilityCalc)segModel.getExclusionModel(rupSet, null));
+//				else
 					builder.adjustTargetsForData(new ThresholdAveragingSectNuclMFD_Estimator.RelGRWorstJumpProb(segModelCalc, 100, true));
 			}
 			builder.applyDefModelUncertainties(false);
