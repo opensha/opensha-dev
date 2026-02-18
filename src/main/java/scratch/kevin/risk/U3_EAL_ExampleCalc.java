eb859355915316faa14b4ae5d081c34b4884a847
Kevin Milner <kmilner@usgs.gov>
2026-02-17 16:38:12 -0800

    new toy case and option to write exceedance probs

--- src/main/java/scratch/kevin/risk/U3_EAL_ExampleCalc.java
+++ src/main/java/scratch/kevin/risk/U3_EAL_ExampleCalc.java
@@ -2,12 +2,18 @@ package scratch.kevin.risk;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.opensha.commons.data.CSVFile;
 import org.opensha.commons.data.Site;
+import org.opensha.commons.data.TimeSpan;
+import org.opensha.commons.data.function.ArbitrarilyDiscretizedFunc;
+import org.opensha.commons.geo.Location;
 import org.opensha.commons.param.Parameter;
 import org.opensha.commons.param.ParameterList;
+import org.opensha.sha.earthquake.AbstractERF;
+import org.opensha.sha.earthquake.PointSource;
 import org.opensha.sha.earthquake.ProbEqkRupture;
 import org.opensha.sha.earthquake.ProbEqkSource;
 import org.opensha.sha.earthquake.param.IncludeBackgroundOption;
@@ -16,8 +22,11 @@ import org.opensha.sha.earthquake.param.ProbabilityModelOptions;
 import org.opensha.sha.earthquake.param.ProbabilityModelParam;
 import org.opensha.sha.imr.AttenRelRef;
 import org.opensha.sha.imr.ScalarIMR;
+import org.opensha.sha.magdist.GutenbergRichterMagFreqDist;
+import org.opensha.sha.util.FocalMech;
 import org.opensha.sra.gui.portfolioeal.Asset;
 import org.opensha.sra.gui.portfolioeal.PortfolioParser;
+import org.opensha.sra.vulnerability.VulnerabilityFetcher;
 
 import com.google.common.base.Preconditions;
 
@@ -26,7 +35,7 @@ import scratch.UCERF3.erf.mean.MeanUCERF3.Presets;
 
 public class U3_EAL_ExampleCalc {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
 		// this is a pretty weird setup; you instantiate the asset by first giving a comma separated list of parameters
 		String assetParamNames = "AssetGroup,AssetID,AssetName,Lat,Lon,ValHi,ValLo,Value,VulnModel,Vs30";
 		// then will pass the values in to the asset; here's a sample from one of Keith's files
@@ -41,9 +50,14 @@ public class U3_EAL_ExampleCalc {
 		
 		// Vulnerability models can be loaded in Keith's 'VUL06' file format. Here's how you could do that
 //		VulnerabilityFetcher.getVulnerabilities(new File("/path/to/vul06.csv"));
+		// here's how I call it to preload the vulnerabilities and skip the servlet; uncomment and change to your path
+		VulnerabilityFetcher.getVulnerabilities(new File("/home/kevin/OpenSHA/portfolio_lec/2011_11_07_VUL06.txt"));
 		// or you can put in your own custom implementation like this. we need to make this better...
 //		Vulnerability vuln = new SimpleVulnerability(name, shortName, imType, imLevels, mfdVals, covVals);
 //		VulnerabilityFetcher.getVulnerabilities().put("MyVulnName", vuln);
+		
+		// if true, will also write conditional probability of exceedance for each IML for each rupture
+		boolean writeIndividualRupExceedances = true;
 		
 		// output file
 		File outputCSV = new File("/tmp/eal_demo.csv");
@@ -59,13 +73,57 @@ public class U3_EAL_ExampleCalc {
 		asset.setAssetParameters(assetValueList);
 		
 		// now we need an ERF, lets use UCERF3 (FM3.1, excluding background seismicity)
-		MeanUCERF3 erf = new MeanUCERF3();
-		erf.setPreset(Presets.FM3_1_BRANCH_AVG);
-		erf.setParameter(IncludeBackgroundParam.NAME, IncludeBackgroundOption.EXCLUDE);
-		// set to poisson, 1 year
-		erf.setParameter(ProbabilityModelParam.NAME, ProbabilityModelOptions.POISSON);
-		erf.getTimeSpan().setDuration(1d);
-		// build the forecast; this will download a data file the first time
+//		MeanUCERF3 erf = new MeanUCERF3();
+//		erf.setPreset(Presets.FM3_1_BRANCH_AVG);
+//		erf.setParameter(IncludeBackgroundParam.NAME, IncludeBackgroundOption.EXCLUDE);
+//		// set to poisson, 1 year
+//		erf.setParameter(ProbabilityModelParam.NAME, ProbabilityModelOptions.POISSON);
+//		erf.getTimeSpan().setDuration(1d);
+		// or use a simple test ERF with a GR point source at a single location
+		// M5-8 range, shows some discrepancy at present
+		double minMag = 5d;
+		double deltaMag = 0.1d;
+		int numMag = 30; // up to M8
+		// single M7 test, matches nearly perfectly
+//		double minMag = 7d;
+//		double deltaMag = 0.1d; // not used
+//		int numMag = 1;
+		
+		GutenbergRichterMagFreqDist grMFD = new GutenbergRichterMagFreqDist(minMag, numMag, deltaMag);
+		Location srcLoc = new Location(37, -122); // make sure this is nearish to to your asset
+		double totRate = 1d; // total cumulative event rate
+		double b = 1d;
+		double duration = 1d;
+		grMFD.setAllButTotMoRate(grMFD.getMinX(), grMFD.getMaxX(), totRate, b);
+		PointSource ptSrc = PointSource.poissonBuilder(srcLoc).truePointSources()
+				.forMFDAndFocalMech(grMFD, FocalMech.STRIKE_SLIP.mechanism).duration(duration).build();
+		System.out.println("Simple ERF ruptures:");
+		for (ProbEqkRupture rup : ptSrc)
+			System.out.println("\tM"+(float)rup.getMag()+" with P="+(float)rup.getProbability()+" and rate="+(float)rup.getMeanAnnualRate(duration));
+		AbstractERF erf = new AbstractERF() {
+			
+			@Override
+			public String getName() {
+				return null;
+			}
+			
+			@Override
+			public void updateForecast() {
+				if (this.timeSpan == null)
+					this.timeSpan = new TimeSpan(TimeSpan.NONE, TimeSpan.YEARS);
+			}
+			
+			@Override
+			public ProbEqkSource getSource(int idx) {
+				Preconditions.checkState(idx == 0, "only 1 source");
+				return ptSrc;
+			}
+			
+			@Override
+			public int getNumSources() {
+				return 1;
+			}
+		};
 		erf.updateForecast();
 		
 		// max source-site dist in KM
@@ -73,6 +131,9 @@ public class U3_EAL_ExampleCalc {
 		
 		// also need a GMM, let's use ASK 2014
 		ScalarIMR gmm = AttenRelRef.ASK_2014.get();
+		// or could use the full NSHM23-WUS model:
+//		ScalarIMR gmm = AttenRelRef.USGS_NSHM23_ACTIVE.get();
+		
 		gmm.setParamDefaults();
 		
 		// this exists in order to supply all required GMM paremeters, e.g. basin depth, that aren't included in the asset
@@ -85,12 +146,34 @@ public class U3_EAL_ExampleCalc {
 		double[][] rupConditionalLosses = asset.calculateExpectedLossPerRup(gmm, maxDistance, null, site, erf, null);
 		double eal = asset.getAssetEAL();
 		
-		System.out.println("EAL: "+eal);
+		System.out.println("EAL from conditional losses: "+eal);
+		
+		// compute it the other way
+		double calcEAL = asset.calculateEAL(gmm, maxDistance, site, erf, null);
+		System.out.println("EAL from the curve: "+calcEAL);
+		
+		System.out.println("Difference: "+Math.abs(eal-calcEAL)+" ("+Math.abs(100d*(eal-calcEAL)/calcEAL)+" %)");
 		
 		// lets write out the individual losses to a CSV File
 		CSVFile<String> csv = new CSVFile<>(false);
 		
-		csv.addLine("Source ID", "Rupture ID", "Magnitude", "Rate", "Conditional Loss");
+		List<String> header = new ArrayList<>();
+		header.add("Source ID");
+		header.add("Rupture ID");
+		header.add("Magnitude");
+		header.add("Rate");
+		header.add("Conditional Loss");
+		ArbitrarilyDiscretizedFunc logXVals = null;
+		if (writeIndividualRupExceedances) {
+			logXVals = new ArbitrarilyDiscretizedFunc();
+			double[] imls = asset.getVulnModel().getIMLValues();
+			header.add(gmm.getShortName()+" Conditional Exceedance Probs");
+			for (double iml : imls) {
+				header.add((float)iml+"");
+				logXVals.set(Math.log(iml), 0d);
+			}
+		}
+		csv.addLine(header);
 		for (int sourceID=0; sourceID<erf.getNumSources(); sourceID++) {
 			if (rupConditionalLosses[sourceID] == null)
 				// too far away
@@ -99,7 +182,20 @@ public class U3_EAL_ExampleCalc {
 			for (int rupID=0; rupID<source.getNumRuptures(); rupID++) {
 				ProbEqkRupture rup = source.getRupture(rupID);
 				double rate = rup.getMeanAnnualRate(erf.getTimeSpan().getDuration());
-				csv.addLine(sourceID+"", rupID+"", (float)rup.getMag()+"", rate+"", rupConditionalLosses[sourceID][rupID]+"");
+				List<String> line = new ArrayList<>(header.size());
+				line.add(sourceID+"");
+				line.add(rupID+"");
+				line.add((float)rup.getMag()+"");
+				line.add(rate+"");
+				line.add(rupConditionalLosses[sourceID][rupID]+"");
+				if (writeIndividualRupExceedances) {
+					line.add(""); // intentionally blank
+					gmm.setEqkRupture(rup);
+					gmm.getExceedProbabilities(logXVals);
+					for (int i=0; i<logXVals.size(); i++)
+						line.add((float)logXVals.getY(i)+"");
+				}
+				csv.addLine(line);
 			}
 		}
 		
