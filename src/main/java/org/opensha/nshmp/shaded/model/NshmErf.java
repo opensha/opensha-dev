fffc00e8dc7a5d0406059d90f0588a1251a9f9f9
Kevin Milner <kmilner@usgs.gov>
2026-08-06 15:35:48 -0700

    now detects and handles sources with TRT-specific-GMM overrides

--- src/main/java/org/opensha/nshmp/shaded/model/NshmErf.java
+++ src/main/java/org/opensha/nshmp/shaded/model/NshmErf.java
@@ -11,22 +11,28 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.apache.commons.numbers.core.Precision;
 import org.opensha.commons.data.TimeSpan;
 import org.opensha.sha.earthquake.AbstractERF;
 import org.opensha.sha.earthquake.ProbEqkSource;
 import org.opensha.sha.earthquake.param.IncludeBackgroundOption;
+import org.opensha.sha.imr.attenRelImpl.nshmp.NSHMP_GMM_Wrapper;
 import org.opensha.sha.util.TectonicRegionType;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.MultimapBuilder;
 
 import org.opensha.nshmp.shaded.data.NshmpIndexing;
+import org.opensha.nshmp.shaded.gmm.NshmpGmm;
 import org.opensha.nshmp.shaded.model.NshmpSystemRuptureSet.SystemRupture;
 import org.opensha.nshmp.shaded.tree.NshmpBranch;
 
@@ -43,6 +49,7 @@ public class NshmErf extends AbstractERF {
 	private final boolean volcanic;
 	private final boolean grid;
 	private final boolean faults;
+	private Set<TectonicRegionType> trts;
 
 	public NshmErf(Path path, Set<TectonicRegionType> trts,
 			IncludeBackgroundOption gridOption) {
@@ -52,6 +59,7 @@ public class NshmErf extends AbstractERF {
 	public NshmErf(NshmpHazardModel model, Set<TectonicRegionType> trts,
 			IncludeBackgroundOption gridOption) {
 		this.model = model;
+		this.trts = trts;
 		allSources = new ArrayList<>();
 		sourceMap = MultimapBuilder
 				.enumKeys(TectonicRegionType.class)
@@ -69,6 +77,8 @@ public class NshmErf extends AbstractERF {
 
 		init();
 	}
+	
+	private static final boolean PROCESS_TRT_OVERRIDES = true;
 
 	private void init() {
 
@@ -79,37 +89,124 @@ public class NshmErf extends AbstractERF {
 
 		// nshmp-haz initializers
 		Multimap<NshmpTectonicSetting, NshmpSourceTree> trees = model.trees();
-		for (Entry<NshmpTectonicSetting, NshmpSourceTree> entry : trees.entries()) {
+		if (PROCESS_TRT_OVERRIDES) {
+			List<NshmpTRTBranch> trtMappedRuptureSets = parseTRTOverrides(trees);
+			for (NshmpTRTBranch trtBranch : trtMappedRuptureSets) {
+				if (!trts.isEmpty() && !trts.contains(trtBranch.trt))
+					continue;
+				List<NshmSource> sources = initBranch(trtBranch.branch);
+				sources.forEach(s -> s.setTectonicRegionType(trtBranch.trt));
+				allSources.addAll(sources);
+				sourceMap.putAll(trtBranch.trt, sources);
+			}
+			allSources.sort(new Comparator<NshmSource>() {
+				@Override
+				public int compare(NshmSource o1, NshmSource o2) {
+					return Integer.compare(o1.getNSHM_ID(), o2.getNSHM_ID());
+				}
+			});
+		} else {
+			for (Entry<NshmpTectonicSetting, NshmpSourceTree> entry : trees.entries()) {
+				NshmpTectonicSetting setting = entry.getKey();
+				NshmpSourceTree tree = entry.getValue();
+				NshmpSourceType type = tree.type();
 
+				TectonicRegionType trt = NshmUtil.tectonicSettingToType(setting, type);
+				if (!trts.isEmpty() && !trts.contains(trt))
+					continue;
+				List<NshmSource> sources = initTree(tree);
+				sources.forEach(s -> s.setTectonicRegionType(trt));
+				allSources.addAll(sources);
+				sourceMap.putAll(trt, sources);
+			}
+		}
+	}
+	
+	private record NshmpTRTBranch(TectonicRegionType trt, NshmpBranch<NshmpRuptureSet> branch) {}
+	
+	/**
+	 * Map each branch to a tectonic region type, also processing any GMM-overrides that Peter might sneak in there
+	 * to change the TRT within a different NshmpTectonicSetting
+	 * @param trees
+	 * @return
+	 */
+	private static List<NshmpTRTBranch> parseTRTOverrides(
+			Multimap<NshmpTectonicSetting, NshmpSourceTree> trees) {
+		List<NshmpTRTBranch> ret = new ArrayList<>();
+		
+		for (Entry<NshmpTectonicSetting, NshmpSourceTree> entry : trees.entries()) {
 			NshmpTectonicSetting setting = entry.getKey();
 			NshmpSourceTree tree = entry.getValue();
 			NshmpSourceType type = tree.type();
-
-			if (setting == NshmpTectonicSetting.SUBDUCTION) {
-				if ((type == NshmpSourceType.INTERFACE || type == NshmpSourceType.INTERFACE_CLUSTER
-						|| type == NshmpSourceType.INTERFACE_GRID || type == NshmpSourceType.INTERFACE_SYSTEM) && !subInterface) {
+			TectonicRegionType origTRT = NshmUtil.tectonicSettingToType(setting, type);
+			
+			for (NshmpBranch<NshmpRuptureSet> rs : tree) {
+				if (origTRT == VOLCANIC) {
+					// copy over as is, no current GMM type for volcanic
+					ret.add(new NshmpTRTBranch(origTRT, rs));
 					continue;
 				}
-				if ((type == NshmpSourceType.SLAB || type == NshmpSourceType.INTRASLAB_GRID) && !subSlab) {
-					continue;
+				NshmpGmmTree gmmTree = rs.value().gmmTree();
+				double weightOrig = 0d;
+				double weightOverrideSum = 0d;
+				Map<TectonicRegionType, Double> overrideWeights = new EnumMap<>(TectonicRegionType.class);
+				for (NshmpBranch<NshmpGmm> gmm : gmmTree.tree()) {
+					TectonicRegionType gmmTRT = NSHMP_GMM_Wrapper.trtForType(gmm.value().type());
+					if (gmmTRT != origTRT) {
+						// we have a GMM TRT override
+						weightOverrideSum += gmm.weight();
+						if (overrideWeights.containsKey(gmmTRT))
+							overrideWeights.put(gmmTRT, overrideWeights.get(gmmTRT)+gmm.weight());
+						else
+							overrideWeights.put(gmmTRT, gmm.weight());
+					} else {
+						weightOrig += gmm.weight();
+					}
+				}
+				if (weightOverrideSum > 0d) {
+					// we have overrides to process
+					Preconditions.checkState(Precision.equals(weightOverrideSum + weightOrig, 1d, 1e-4));
+					if (weightOrig > 0d)
+						ret.add(new NshmpTRTBranch(origTRT, new WeightScaledBranch(rs, weightOrig, origTRT)));
+					for (TectonicRegionType trt : overrideWeights.keySet())
+						ret.add(new NshmpTRTBranch(trt, new WeightScaledBranch(rs, overrideWeights.get(trt), trt)));
+				} else {
+					// copy over as is
+					ret.add(new NshmpTRTBranch(origTRT, rs));
 				}
 			}
-			if (setting == NshmpTectonicSetting.STABLE_CRUST && !stableCrust) {
-				continue;
-			}
-			if (setting == NshmpTectonicSetting.ACTIVE_CRUST && !activeCrust) {
-				continue;
-			}
-			if (setting == NshmpTectonicSetting.VOLCANIC && !volcanic) {
-				continue;
-			}
-
-			TectonicRegionType trt = NshmUtil.tectonicSettingToType(setting, type);
-			List<NshmSource> sources = initTree(tree);
-			sources.forEach(s -> s.setTectonicRegionType(trt));
-			allSources.addAll(sources);
-			sourceMap.putAll(trt, sources);
 		}
+		
+		return ret;
+	}
+	
+	private static class WeightScaledBranch implements NshmpBranch<NshmpRuptureSet> {
+		
+		private NshmpBranch<NshmpRuptureSet> upstream;
+		private double scale;
+		private TectonicRegionType trt;
+
+		public WeightScaledBranch(NshmpBranch<NshmpRuptureSet> upstream, double scale, TectonicRegionType trt) {
+			this.upstream = upstream;
+			this.scale = scale;
+			this.trt = trt;
+		}
+
+		@Override
+		public String id() {
+			return upstream.id()+"-"+trt.name();
+		}
+
+		@Override
+		public NshmpRuptureSet value() {
+			return upstream.value();
+		}
+
+		@Override
+		public double weight() {
+			return upstream.weight()*scale;
+		}
+		
 	}
 
 	public List<NshmSource> allSources() {
@@ -148,6 +245,14 @@ public class NshmErf extends AbstractERF {
 				return Integer.compare(o1.getNSHM_ID(), o2.getNSHM_ID());
 			}
 		});
+
+		return sources;
+	}
+
+	private List<NshmSource> initBranch(NshmpBranch<NshmpRuptureSet> branch) {
+		List<NshmSource> sources = new ArrayList<>();
+		double duration = getTimeSpan().getDuration();
+		sources.addAll(sourcesFromBranch(branch, duration));
 
 		return sources;
 	}
